5fdb6c0f87d429a2e2ee47f7620248c89a2811a4
CSS2919 <silva.christian@gmail.com>
2022-09-08 10:42:38 -0300

    segundo commit

--- src/main/java/com/pos/cifraCesar/repository/CifraRepository.java
+++ src/main/java/com/pos/cifraCesar/repository/CifraRepository.java
@@ -1,10 +1,9 @@
 package com.pos.cifraCesar.repository;
 
+import com.pos.cifraCesar.model.CifraModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface CifraRepository extends JpaRepository<com.pos.cifraCesar.model.CifraModel, Long> {
-
+public interface CifraRepository extends JpaRepository<CifraModel, Long> {
 }
